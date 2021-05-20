53e27214f53520e9ff831aa8a9714c1e5c4aa016
kefan90 <1554315282@qq.com>
2021-05-20 17:06:04 +0800

    添加路由

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 
 import 'swiper/dist/css/swiper.min.css'
 import 'swiper/dist/js/swiper.min'
@@ -9,6 +10,7 @@ import 'swiper/dist/js/swiper.min'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   components: { App },
   template: '<App/>'
 })
